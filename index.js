95dbc7d6efad947706b9f8a7b44084ff916a48d2
Gabriel <shalitmontagne.g@gmail.com>
2021-10-10 20:53:06 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(drivers, driver) {
+    return drivers.filter(function (name) {
+        return name.toLowerCase() === driver.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, s) {
+    return drivers.filter(function (name) {
+        return name.startsWith(s)
+    });
+}
+
+function matchName(drivers, driver) {
+    return drivers.filter(function (n) {
+        return n.name === driver
+    });
+}
